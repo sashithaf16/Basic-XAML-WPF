155f6ebbe422daf37a9970c46b7f6362037e7f67
Sashitha Fonseka <sashithadfonseka@gmail.com>
2019-07-08 09:13:36 +0530

    Data Binding - One Way

---
+++ DataBiding/OneWayDataBiding/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneWayDataBiding
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+        public static Employee GetEmployee()
+        {
+            var emp = new Employee()
+            {
+                Name = "Devin",
+                Title = "Developer"
+            };
+            return emp;
+        }
+
+    }
+}
